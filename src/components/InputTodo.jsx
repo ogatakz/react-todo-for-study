e371b5a0b9c977d7346f277203184fbd4e438eed
ogatakz <100104512+ogatakz@users.noreply.github.com>
2022-02-23 22:37:45 +0900

    tried Css-in-js and improvement

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 
+// javascriptで書く場合は、キャメルケースで書く必要がある。
+// cssと違い要素で書く必要があるので「""」で括る事と「;を,」への修正を実施する。
+const style = {
+  backgroundColor: "#c1ffff",
+  width: "400px",
+  height: "30px",
+  padding: "8px",
+  margin: "8px",
+  borderRadius: "8px"
+};
+
 // inputエリアのコンポーネント化
 export const InputTodo = (props) => {
-  const { todoText, onChange, onClick } = props;
+  const { todoText, onChange, onClick, disabled } = props;
   return (
-    <div className="input-area">
-      <input placeholder="TODOを入力" value={todoText} onChange={onChange} />
-      <button onClick={onClick}>追加</button>
+    // <div className="input-area">
+    <div style={style}>
+      <input
+        disabled={disabled}
+        placeholder="TODOを入力"
+        value={todoText}
+        onChange={onChange}
+      />
+      <button disabled={disabled} onClick={onClick}>
+        追加
+      </button>
     </div>
   );
 };
